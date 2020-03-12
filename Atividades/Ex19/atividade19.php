aef8bf0c28860494416dd1882286af5e4361017c
Wes1738 <wesleyrodriguessantos99@gmail.com>
2020-03-12 14:06:32 -0300

    Atividade 19 atualizada com correções

--- Atividades/Ex19/atividade19.php
+++ Atividades/Ex19/atividade19.php
@@ -10,13 +10,10 @@
 
 		if	   ($num1 > $num2 && $num2 > $num3) echo "$num1 $num2 $num3";
 		elseif ($num3 > $num2 && $num2 > $num1) echo "$num3 $num2 $num1";
-		elseif ($num1 == $num2 && $num1 == $num3) echo "$num1 $num2 $num3"; 
 		elseif ($num2 > $num1 && $num1 > $num3) echo "$num2 $num1 $num3";
 		elseif ($num2 > $num1 && $num1 < $num3) echo "$num2 $num3 $num1";
-		elseif ($num2 == $num1 && $num1 < $num3) echo "$num3 $num2 $num1";
-		elseif ($num2 == $num1 && $num1 > $num3) echo "$num1 $num2 $num3";
-		elseif ($num3 == $num1 && $num2 > $num3) echo "$num2 $num1 $num3";
-		elseif ($num3 == $num1 && $num2 < $num3) echo "$num2 $num1 $num2";
+		elseif ($num1 > $num3 && $num3 > $num2) echo "$num1 $num3 $num2";
+		elseif ($num3 > $num1 && $num1 > $num2) echo "$num3 $num1 $num2";
 	}
 
  ?>
