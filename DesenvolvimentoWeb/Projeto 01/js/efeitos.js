abd330eff7d283fdf18df89d80435ebe631bf606
Wes1738 <wesleyrodriguessantos99@gmail.com>
2020-03-25 19:08:13 -0300

    Menu Mobile parte 3, quase finalizado

--- DesenvolvimentoWeb/Projeto 01/js/efeitos.js
+++ DesenvolvimentoWeb/Projeto 01/js/efeitos.js
@@ -54,4 +54,23 @@ $(document).ready(function(){
 
 			});
 
+			$("#btn-bars").on("click", function(){
+
+				$("header").toggleClass("open-menu");
+
+			});
+
+			$("#menu-mobile-mask, .btn-close").on("click", function(){
+
+				$("header").removeClass("open-menu");
+
+			});
+
+			$("#btn-search").on("click", function(){
+
+				$("header").toggleClass("open-search");
+				$("#input-search-mobile").focus();
+
+			});
+
 		});
