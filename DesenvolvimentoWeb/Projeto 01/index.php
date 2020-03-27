c9ebb4f47df5efee2bf7732e217aee00533a7f7b
Wes1738 <wesleyrodriguessantos99@gmail.com>
2020-03-27 12:17:37 -0300

    Atualização do arquivo index.html para index.php, conexão agora com o banco de dados mysqli e pasta inc adicionada para incluir arquivos de configuração posteriores

--- DesenvolvimentoWeb/Projeto 01/index.php
+++ DesenvolvimentoWeb/Projeto 01/index.php
@@ -1,3 +1,20 @@
+<?php 
+	
+require_once("inc/configuration.php");
+
+$sql = new Sql();
+
+$result = $sql->query("SELECT * FROM tb_produtos;");
+
+while ($row = mysqli_fetch_array($result)) {
+	
+	var_dump($row);
+
+}
+
+exit;
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
