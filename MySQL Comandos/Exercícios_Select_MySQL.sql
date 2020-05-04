0162e9c00ba232c05f5f5f45f0c4f57f45d118a2
Wesley Rodrigues dos Santos <wesleyrodriguessantos99@gmail.com>
2020-05-04 10:10:18 -0300

    Exercícios sobre SELECT

---
+++ MySQL Comandos/Exercícios_Select_MySQL.sql
@@ -0,0 +1,22 @@
+/*Exercícios sobre SELECT no MySQL*/
+
+/* Uma lista com o nome de todas as gafanhotas */
+SELECT nome from gafanhotos where sexo = 'F';
+
+/* Uma lista com com os dados de todos aqueles que nasceram entre 1/Jan/2000 e 31/dez/2015 */
+SELECT * FROM gafanhotos WHERE nascimento BETWEEN '2000-01-01' and '2015-12-31';
+
+/* Uma lista com o nome de todos os homens que trabalham como Programadores */
+SELECT nome from gafanhotos where profissao = 'Programador' and sexo = 'M' order by nome;
+
+/* Uma lista com todos os dados de  todas as mulheres que nasceram no Brasil e que tem seu nome iniciando com a letra J */ 
+SELECT * from gafanhotos where nacionalidade = 'Brasil' and  nome like 'J%' ORDER BY nome;
+
+/* Uma lista com o nome e a nacionalidade de todos os homens que têm Silva no nome, não nasceram no Brasil e pesam menos de 100Kg */
+SELECT nome, nacionalidade from gafanhotos where sexo = 'M' AND nacionalidade <> 'Brasil' and peso < '100' and nome like '%Silva%' order by nome;
+
+/* Qual é a maior altura entre gafanhotos homens que moram no Brasil? */
+SELECT max(altura) from gafanhotos where sexo = 'M' and nacionalidade = 'Brasil';
+
+/* Qual é a média de peso dos gafanhotos cadastrados? */
+
