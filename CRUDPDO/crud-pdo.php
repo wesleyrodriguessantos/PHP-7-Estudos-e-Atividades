a107e698c78575ef5603836093083d147d965683
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-05-06 20:55:37 -0300

    Aula 02 - INSERT

--- CRUDPDO/crud-pdo.php
+++ CRUDPDO/crud-pdo.php
@@ -1,5 +1,6 @@
 <?php
 
+//-----------------------------CONEXÃO--------------------------------------
 try {
     $pdo = new PDO("mysql:dbname=CRUDPDO;host=localhost", "root", "");
 } catch (PDOException $e) {
@@ -9,5 +10,18 @@ catch (Exception $e) {
     echo "Erro Genérico ". $e->getMessage();
 }
 
+//----------------------------INSERT------------------------------------------
+
+// 1ª Maneira - Usando Prepare
+
+/*$res = $pdo->prepare("INSERT INTO pessoa (nome, telefone, email) VALUES (:n, :t, :e)");
+$res->bindValue(":n", "Ana Carolina");
+$res->bindValue(":t", "62987654544");
+$res->bindValue(":e", "carol@outlook.com.br");
+$res->execute();*/
+
+//2ª Maneira - Usando Query
+
+$pdo->query("INSERT INTO pessoa (nome, telefone, email) VALUES ('Rosângela', '64985253214', 'rosa@flor.br')");
 
 ?>
