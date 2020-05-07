95a1e94b43a358a6dc1db77212292964914ec544
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-05-06 21:36:28 -0300

    Aula 03 - DELETE e UPDATE

--- CRUDPDO/crud-pdo.php
+++ CRUDPDO/crud-pdo.php
@@ -22,6 +22,28 @@ $res->execute();*/
 
 //2ª Maneira - Usando Query
 
-$pdo->query("INSERT INTO pessoa (nome, telefone, email) VALUES ('Rosângela', '64985253214', 'rosa@flor.br')");
+//$pdo->query("INSERT INTO pessoa (nome, telefone, email) VALUES ('Rosângela', '64985253214', 'rosa@flor.br')");
+
+//----------------------------DELETE E UPDATE------------------------------------------
+
+// 1ª Maneira - Usando Prepare
+/*$cmd = $pdo->prepare("DELETE FROM pessoa Where id = :id");
+$id = 4;
+$cmd->bindValue(":id", $id);
+$cmd->execute();*/
+
+//2ª Maneira - Usando Query
+
+//$res = $pdo->query("DELETE FROM pessoa WHERE id = '5'");
+
+// 1ª Maneira - Usando Prepare
+/*$cmd = $pdo->prepare("UPDATE pessoa SET email = :em WHERE id = :id");
+$cmd->bindValue(":em", "Novoemail@brasil.com.br");
+$cmd->bindValue(":id", 2);
+$cmd->execute();*/
+
+//2ª Maneira - Usando Query
+$res = $pdo->query("UPDATE pessoa SET email = 'Rosangeladias@hotmail.com' WHERE id = '3'");
+
 
 ?>
