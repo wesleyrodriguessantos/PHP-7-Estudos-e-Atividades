a3b09c2254e4a4f0cf4fe01584cb2d5745cf11b7
Wesley Rodrigues <wesleyrodriguessantos99@gmail.com>
2020-05-07 21:49:16 -0300

    Aula 05 - Front-end do Projeto

---
+++ CRUD-PESSOA/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro Pessoa</title>
+    <link rel="stylesheet" href="estilo.css" >
+</head>
+<body>
+    <section id="esquerda">
+    <form>
+            <h2>CADASTRAR PESSOA</h2>
+            <label for="nome">NOME</label>
+            <input type="text" name="nome" id="nome">
+            <label for="telefone">TELEFONE</label>
+            <input type="text" name="telefone" id="telefone">
+            <label for="email">EMAIL</label>
+            <input type="text" name="email" id="email">
+            <input type="submit" value="Cadastrar">
+        </form>
+    </section>
+    <section id="direita">
+        <table>
+            <tr id="titulo">
+                <td>Nome</td>
+                <td>Telefone</td>
+                <td colspan="2">Email</td>
+            </tr>
+            <tr>
+                <td>Joelma</td>
+                <td>5236363636</td>
+                <td>Joelma5656@gmail.com</td>
+                <td><a href="#">Editar</a><a href="#">Excluir</a></td>
+            </tr>
+        </table>
+    </section>
+    
+</body>
+</html>
